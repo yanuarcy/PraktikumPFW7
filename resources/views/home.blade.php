87c50d94a8b86c781f9fd4b00a8b48fdf590a238
Yanuar <yanuarcahyo567@gmail.com>
2023-06-20 07:00:18 +0700

    Pembuatan HomePage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('Layouts.app')
+
+@section('content')
+    @include('default')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::get('profile', ProfileController::class)->name('profile');
+
+Route::resource('employees', EmployeeController::class);
+
